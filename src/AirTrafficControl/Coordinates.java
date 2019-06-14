2a891c3f0ad77a396953d30aa0ccab11b5de7dd5
kmarchan <kmarchan@student.wethinkcode.co.za>
2019-06-14 20:03:26 +0200

    wip

---
+++ src/AirTrafficControl/Coordinates.java
@@ -0,0 +1,32 @@
+package AirTrafficControl;
+
+public class Coordinates {
+    private int longitude;
+    private int latitude;
+    private int height;
+
+
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
+    }
+}
---
+++ src/Hanger/Aircraft.java
@@ -0,0 +1,46 @@
+package Hanger;
+
+import AirTrafficControl.Coordinates;
+
+public class Aircraft {
+
+    protected int id;
+    protected String name;
+    protected Coordinates coordinates;
+    private long idCounter;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    protected Aircraft(String name, Coordinates coordinates) {
+        this.setName(name);
+        this.setCoordinates(coordinates);
+    }
+
+    public long nextId() {
+        return this.idCounter++;
+    }
+
+}
+6
