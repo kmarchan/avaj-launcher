02de37f99dffa4f87f20934eace7d8097e94b40a
kmarchan <kmarchan@student.wethinkcode.co.za>
2019-06-17 13:22:53 +0200

    wip

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+            if (args.length != 1) { System.out.println("This program will take one and only one argument");}
+
     }
 }
